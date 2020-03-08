6b6b8726a25956f4ee48d773d4e64228bc9280c6
akhil <akhilkhubchandani@gmail.com>
2020-03-08 15:15:56 +0530

    Added two more activities

--- app/src/main/java/com/example/login/SecondActivity.java
+++ app/src/main/java/com/example/login/SecondActivity.java
@@ -2,13 +2,35 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SecondActivity extends AppCompatActivity {
+    FloatingActionButton addNote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        addNote=findViewById(R.id.addnote);
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newNote();
+            }
+        });
     }
+
+    private void newNote()
+    {
+        Intent intent=new Intent(SecondActivity.this,ThirdActivity.class);
+        startActivity(intent);
+    }
+
+
 }
